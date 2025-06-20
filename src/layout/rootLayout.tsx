2d377812c2368a585fbbf6c1e350690f8a7094eb
jjlabrada <jjlabranate@gmail.com>
2025-06-20 14:18:53 -0400

    theme config

--- src/layout/rootLayout.tsx
+++ src/layout/rootLayout.tsx
@@ -1,11 +1,26 @@
+import useGamorStore from "@/store/store";
+import { useEffect } from "react";
 import { Outlet } from "react-router";
 
 const RootLayout = () => {
+  const { theme } = useGamorStore();
+
+   useEffect(() => {
+    let applied = theme;
+    if (theme === 'system') {
+      applied = window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'dark'
+        : 'light';
+    }
+
+    document.documentElement.classList.toggle('dark', applied === 'dark');
+  }, [theme]);
+
   return (
-      <main className="relative bg-background text-text min-h-screen w-full">
-        <Outlet/>
-      </main>
-  );  
+    <main className="relative bg-background text-text min-h-screen w-full">
+      <Outlet />
+    </main>
+  );
 };
 
 export default RootLayout;
