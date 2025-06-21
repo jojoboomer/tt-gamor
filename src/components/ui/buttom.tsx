bbc92293877cb1a1d478da9419946efc6ce754ee
jjlabrada <jjlabranate@gmail.com>
2025-06-21 01:40:56 -0400

    feat: implement button component with variant and size options

--- src/components/ui/buttom.tsx
+++ src/components/ui/buttom.tsx
@@ -1,51 +1,126 @@
 import { cn } from "@/lib/utils";
+import { cva } from "class-variance-authority";
 
-type Variants = "primary" | "secondary" | "ghost" | "link" | "icon";
+const buttonVariants = cva(
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default: "",
+        outline: "bg-transparent border",
+        ghost: "hover:bg-black/10 dark:hover:bg-white/10",
+        link: "underline-offset-4 hover:underline",
+        icon: "rounded-full",
+      },
+      color: {
+        default: "text-black dark:text-white",
+        primary: "text-primary",
+        accent: "text-accent",
+      },
+      size: {
+        sm: "h-8 px-3 text-xs",
+        md: "h-9 px-4 py-2",
+        lg: "h-10 px-8",
+        icon: "size-9",
+      },
+    },
+    compoundVariants: [
+      // Default
+      {
+        variant: "default",
+        color: "default",
+        class: "hover:bg-black/80 bg-black text-white dark:hover:bg-white/80 dark:bg-white dark:text-black shadow ",
+      },
+      {
+        variant: "default",
+        color: "primary",
+        class: "hover:bg-primary/80 bg-primary text-white shadow ",
+      },
+      {
+        variant: "default",
+        color: "accent",
+        class: "hover:bg-accent/80 bg-accent text-black shadow ",
+      },
+      // Outline
+      {
+        variant: "outline",
+        color: "default",
+        class: "hover:text-black/80 text-black dark:hover:text-white/80 dark:text-white",
+      },
+      {
+        variant: "outline",
+        color: "primary",
+        class: "hover:text-primary/80 text-primary",
+      },
+      {
+        variant: "outline",
+        color: "accent",
+        class: "hover:opacity-80 text-accent",
+      },
+      // Ghost
+      {
+        variant: "ghost",
+        color: "default",
+        class: "",
+      },
+      {
+        variant: "ghost",
+        color: "primary",
+        class: "text-primary hover:bg-primary/10",
+      },
+      {
+        variant: "ghost",
+        color: "accent",
+        class: "text-accent hover:bg-accent/10",
+      },
+      // Links
+      {
+        variant: "link",
+        color: "default",
+        class: "text-black dark:text-white",
+      },
+      {
+        variant: "link",
+        color: "primary",
+        class: "text-blue-600",
+      },
+      {
+        variant: "link",
+        color: "accent",
+        class: "text-green-600",
+      },
+    ],
+    defaultVariants: {
+      variant: "default",
+      color: "default",
+      size: "md",
+    },
+  },
+)
 
-type Sizes = "sm" | "md" | "lg";
-
-interface Props {
+interface Props extends React.ComponentProps<"button"> {
   children: React.ReactNode;
-  onClick?: () => void;
   className?: string;
-  variant?: Variants;
-  size?: Sizes;
+  variant?: buttonVariants;
+  color?: colorVariants;
+  size?: sizeVariants;
 }
 
 const Button = ({
   children,
-  onClick,
-  variant = "primary",
+  variant = "default",
+  color = "default",
   size = "md",
   className,
   ...props
 }: Props) => {
-  const baseClasses = `inline-flex items-center justify-center hover:cursor-pointer`;
-
-  const variantClasses = {
-    primary: "bg-primary text-white hover:bg-primary/80 shadow-lg rounded-4xl",
-    secondary: "bg-accent text-black hover:bg-accent/10 shadow-lg rounded-4xl",
-    ghost: "bg-transparent text-black hover:bg-black/10 rounded-4xl",
-    link: "bg-transparent",
-    icon: "bg-primary text-white rounded-full aspect-square",
-  };
-
-  const sizeClasses = {
-    sm: "px-3 py-1.5 text-sm",
-    md: "px-4 py-2 text-sm",
-    lg: "px-6 py-3 text-base",
-    icon: "size-10 text-base",
-  };
-
+ 
   return (
     <button
       className={cn(
-        baseClasses,
-        variantClasses[variant],
-        variant == "icon" ? sizeClasses['icon'] : sizeClasses[size],
+        buttonVariants({ variant, color, size, className }),
         className
       )}
-      onClick={onClick}
       {...props}
     >
       {children}
