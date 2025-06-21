500b237b2bb22db534f97ec2e0e249dc723ec284
jjlabrada <jjlabranate@gmail.com>
2025-06-21 01:39:58 -0400

    fix: correct route path for login and format code

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import { Route, Routes } from "react-router"
-import RootLayout from "./layout/rootLayout"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
+import { Route, Routes } from "react-router";
+import RootLayout from "./layout/rootLayout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
-
   return (
     <Routes>
       <Route element={<RootLayout />}>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Route>
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
