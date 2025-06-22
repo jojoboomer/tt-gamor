daee8a6e4998a24e682a1dd3d43165b90eb4ee6e
jjlabrada <jjlabranate@gmail.com>
2025-06-22 18:36:47 -0400

    Add Decoration component for SVG rendering with customizable className

---
+++ src/features/Home/components/decoration.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils";
+
+const Decoration = ({className} : {className?: string}) => {
+  return (
+    <svg
+      height="150"
+      width="300"
+      xmlns="http://www.w3.org/2000/svg"
+      className={cn(className)}
+    >
+      <path
+        stroke="inherit"
+        d="M41 57c-64 35-57 104 56 80 47-11 83-26 116-51 71-59-1-96-68-71"
+        fill="transparent"
+        strokeWidth="1"
+      />
+    </svg>
+  );
+};
+
+export default Decoration;
