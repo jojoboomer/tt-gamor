560cf9c921d120783b77012c54b49d80213a93f7
jjlabrada <jjlabranate@gmail.com>
2025-06-22 01:07:40 -0400

    Update main section styles

--- src/features/Home/components/authSection.tsx
+++ src/features/Home/components/authSection.tsx
@@ -1,4 +1,21 @@
+import { cn } from "@/lib/utils";
 import Button from "@components/ui/buttom";
+
+interface Props extends React.ComponentProps<"button"> {
+  className?: string;
+  error?: boolean;
+}
+
+const CustomAccountButton = ({ className, ...props }: Props) => {
+  return (
+    <button
+      {...props}
+      className={cn("text-sm px-8 py-4 rounded-4xl shadow-lg", className)}
+    >
+      Create account
+    </button>
+  );
+};
 
 const AuthSection = () => {
   return (
@@ -13,21 +30,15 @@ const AuthSection = () => {
         gamor now has{" "}
         <span className="relative font-bold ">
           stream party{" "}
-          <svg className="absolute left-0 w-full -bottom-6 h-8 stroke-accent dark:stroke-primary"  >
-            <path
-              d="M5 20 Q60 5 90 20"
-              fill="transparent"
-              stroke-width="2"
-            />
+          <svg className="absolute left-0 w-full -bottom-6 h-8 dark:stroke-accent stroke-primary">
+            <path d="M5 20 Q60 5 90 20" fill="transparent" strokeWidth="2" />
           </svg>
         </span>
         platform
       </p>
-      <div className="flex gap-4">
-        <Button size="lg" variant="default">
-          Create account
-        </Button>
-        <Button size="md" variant="ghost">
+      <div className="flex gap-6 items-center">
+        <CustomAccountButton className="dark:bg-white/10 dark:hover:bg-white/20 bg-white hover:bg-black/5" />
+        <Button size="md" variant="text">
           Sign in
         </Button>
       </div>
