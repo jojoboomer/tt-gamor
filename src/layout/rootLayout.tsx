374382dd1b83bbd228e1a0b8c85b9b4a92c19fe3
jjlabrada <jjlabranate@gmail.com>
2025-06-22 01:07:24 -0400

    Fix: load game modal and Modal Provider

--- src/layout/rootLayout.tsx
+++ src/layout/rootLayout.tsx
@@ -1,4 +1,6 @@
-import useAuthenticationStore from "@/features/Login/store/auth.store";
+import GameModal from "@/features/Home/components/gameModal";
+import useAuthenticationStore from "@/store/auth.store";
+import { ModalProvider } from "@/store/modal.context";
 import useGamorStore from "@store/ui.store";
 import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router";
@@ -6,30 +8,32 @@ import { Outlet, useNavigate } from "react-router";
 const RootLayout = () => {
   const { theme } = useGamorStore();
   const { session } = useAuthenticationStore();
-  const navigation = useNavigate()
+  const navigation = useNavigate();
 
   useEffect(() => {
-    console.log(session);
     if (!session) {
       navigation("/login");
-    } 
+    }
   }, [theme]);
 
-   useEffect(() => {
+  useEffect(() => {
     let applied = theme;
-    if (theme === 'system') {
-      applied = window.matchMedia('(prefers-color-scheme: dark)').matches
-        ? 'dark'
-        : 'light';
+    if (theme === "system") {
+      applied = window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light";
     }
 
-    document.documentElement.classList.toggle('dark', applied === 'dark');
+    document.documentElement.classList.toggle("dark", applied === "dark");
   }, [theme]);
 
   return (
-    <main className="relative bg-background text-text min-h-screen w-full">
-      <Outlet />
-    </main>
+    <ModalProvider>
+      <main className="relative bg-background text-text min-h-screen w-full">
+        <Outlet />
+      </main>
+      <GameModal />
+    </ModalProvider>
   );
 };
 
