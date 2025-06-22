110f0e794b879360476e049b2e3141969aa24dd4
jjlabrada <jjlabranate@gmail.com>
2025-06-22 18:37:14 -0400

    Add dark mode support to DigitalClock component

--- src/features/Home/components/GameSection/digitalClock.tsx
+++ src/features/Home/components/GameSection/digitalClock.tsx
@@ -22,7 +22,7 @@ const DigitalClock = () => {
   };
 
   return (
-    <div className=" w-fit bg-white text-primary rounded-4xl px-6 py-2 shadow-lg
+    <div className=" w-fit bg-white text-primary dark:text-accent rounded-4xl px-6 py-2 shadow-lg
      text-lg font-semibold text-center tracking-widest">
       {formatTime(time)}
     </div>
