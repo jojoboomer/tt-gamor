9627e1b3b51d31e50e1868deee51053eef7345a5
jjlabrada <jjlabranate@gmail.com>
2025-06-22 01:06:11 -0400

    Rename component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,16 @@
 import AuthSection from "@/features/Home/components/authSection";
+import FilterSection from "@/features/Home/components/filterSection";
 import GameSection from "@/features/Home/components/gameSection";
 import Header from "@/features/Home/components/header";
-import PlayerSection from "@/features/Home/components/playerSection";
 
 const Home = () => {
   return (
     <section className="h-screen w-full px-16 flex flex-col pb-12 ">
       <Header />
-      <div className="h-full overflow-hidden bg-panel flex rounded-2xl ">
+      <div className="h-full overflow-hidden bg-panel flex rounded-2xl">
         <AuthSection />
         <GameSection />
-        <PlayerSection />
+        <FilterSection />
       </div>
     </section>
   );
