636d828612b84a81351d9f3d911d4fe7c3632989
jjlabrada <jjlabranate@gmail.com>
2025-06-23 01:35:07 -0400

    feat: Enhance EventItem and EventList components for improved event interaction

--- src/features/Home/components/FilterSection/eventItem.tsx
+++ src/features/Home/components/FilterSection/eventItem.tsx
@@ -1,40 +1,55 @@
 import Button from "@/components/ui/buttom";
-import { Plus } from "lucide-react";
+import { useEventParticipation } from "@/hooks/useJoinEvents";
+import useGamorStore from "@/store/main.store";
+import { Minus, Plus } from "lucide-react";
+import AvatarStack from "../avatarStack";
 
 type EventItemProps = {
   index: number;
   item: StreamEvent;
-  onClick: () => void;
 };
 
-const ButtomAdd = ({ ...props }) => (
+const ButtomAdd = ({
+  action = "add",
+  ...props
+}: {
+  action?: "add" | "remove";
+} & React.ComponentProps<typeof Button>) => (
   <Button
     variant="default"
     size="sm"
     {...props}
     className="size-6 cursor-pointer !bg-neutral group-hover:scale-110 transition-transform duration-150 ease-out"
   >
-    <Plus className="size-3 text-text" />
+    {action === "add" ? (
+      <Plus className="size-3 text-text" />
+    ) : (
+      <Minus className="size-3 text-text" />
+    )}
   </Button>
 );
 
-export const EventItem = ({ index, item, onClick }: EventItemProps) => (
-  <li key={index} className="flex items-center group gap-4 ">
-    <div className="flex items-center gap-3">
-      <span className="text-sm size-6 rounded-full flex items-center justify-center bg-neutral text-text/80 group-hover:text-text hoi">
-        {index + 1}
-      </span>
-      <span className="font-medium text-text/80 hover:text-text">
-        {item.name}
-      </span>
-    </div>
-    <div className="flex items-center -space-x-2 flex-1 ">
-      {item.streamers.map((streamer) => (
-        <img src={streamer.avatar} alt={streamer.username} className="size-6 rounded-full border border-border"></img>
-      ))}
-    </div>
-    <ButtomAdd onClick={onClick}></ButtomAdd>
-  </li>
-);
+export const EventItem = ({ index, item }: EventItemProps) => {
+  const { toggleEventParticipation } = useEventParticipation();
+  const { currentEvent } = useGamorStore();
+
+  return (
+    <li key={index} className="flex items-center group gap-4 ">
+      <div className="flex items-center gap-3">
+        <span className="text-sm size-6 rounded-full flex items-center justify-center bg-neutral text-text/80 group-hover:text-text hoi">
+          {index}
+        </span>
+        <span className="font-medium text-text/80 hover:text-text">
+          {item.name}
+        </span>
+      </div>
+      <AvatarStack list={item.streamers} />
+      <ButtomAdd
+        action={item.id == currentEvent?.id ? "remove" : "add"}
+        onClick={() => toggleEventParticipation(item.id)}
+      ></ButtomAdd>
+    </li>
+  );
+};
 
 export default EventItem;
--- src/features/Home/components/FilterSection/eventList.tsx
+++ src/features/Home/components/FilterSection/eventList.tsx
@@ -1,17 +1,14 @@
-import { useCallback } from "react";
 import EventItem from "./eventItem";
 
 const EventList = ({ results }) => {
-  const handleAdd = useCallback(() => console.log(), []);
 
   return (
     <ul className=" w-full h-full py-3 space-y-2 overflow-y-auto no-scrollbar flex-1 pb-20">
       {results.map((event, index) => (
         <EventItem
-          key={index.id}
+          key={event.id}
           index={index + 1}
           item={event}
-          onClick={handleAdd}
         />
       ))}
     </ul>
