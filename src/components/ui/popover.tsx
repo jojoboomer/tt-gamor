17b3bbf5a247a2b1b165072eeb594b78d5f6af40
jjlabrada <jjlabranate@gmail.com>
2025-06-23 16:03:16 -0400

    Refactor ReactPopover component to simplify mouse event handling and improve type annotations

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -1,17 +1,16 @@
+import { cn } from "@/lib/utils";
 import { useRef, useState } from "react";
 
-function ReactPopover({ children, content }) {
+function ReactPopover({ children, content } : { children: React.ReactNode, content: React.ReactNode }) {
   const [show, setShow] = useState(false);
   const wrapperRef = useRef(null);
 
   const handleMouseOver = () => {
-    setTimeout(() => {
-      setShow(true);
-    }, 500);
+    setShow(true);
   };
 
   const handleMouseLeft = () => {
-      setShow(false);
+    setShow(false);
   };
 
   return (
@@ -23,8 +22,11 @@ function ReactPopover({ children, content }) {
     >
       <div>{children}</div>
       <div
-        hidden={!show}
-        className="min-w-fit w-fit inline h-fit absolute bottom-[100%] z-50"
+        className={cn(
+          "min-w-fit w-fit inline h-fit absolute bottom-[100%] z-50",
+          "transition-all duration-300 ease-in-out", 
+          show ? "opacity-100 translate-y-0" : "opacity-0 translate-y-2 pointer-events-none" 
+        )}
       >
         <div className="rounded bg-panel opacity-90 p-2 text-sm shadow-md text-text mb-[10px]">
           {content}
