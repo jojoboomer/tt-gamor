d36129d616f44cd87a8126fa30728dc8ab05e16b
jjlabrada <jjlabranate@gmail.com>
2025-06-23 19:23:52 -0400

    Refactor category section

--- src/features/Home/components/CategorySection/categoryCard.tsx
+++ src/features/Home/components/CategorySection/categoryCard.tsx
@@ -5,18 +5,24 @@ import { ArrowRight } from "lucide-react";
 interface Props {
   className?: string;
   category: any;
+  index: number;
 }
-const CategoryCard = ({ className, category }: Props) => {
+const CategoryCard = ({ className, category, index }: Props) => {
   return (
     <Card
-      className={cn("group relative aspect-video w-full lg:w-auto h-40 flex-col-reverse items-end bg-center bg-cover hover:cursor-pointer", className)}
+      className={cn(
+        "group relative aspect-video w-full lg:w-auto h-40 flex-col-reverse items-end bg-center bg-cover hover:cursor-pointer border border-gray-400 dark:border-primary ",
+        className
+      )}
       style={{ backgroundImage: `url(${category.box_art_url})` }}
       key={category.id}
     >
       <div className="absolute z-10 inset-0 rounded-2xl bg-panel group-hover:bg-primary/40 transition-colors duration-200 ease-in-out"></div>
-      <div className=" z-10 inline-flex items-center gap-2 ">
-        <p className="text-xl group-hover:text-white">{category.name}</p>
-        <ArrowRight className="relative w-0 -left-8 group-hover:w-fit group-hover:left-0 transition-all duration-200 ease-out" />
+      <div className="w-full h-full z-10 flex flex-col gap-2 text-xl justify-between ">
+        <div className="text-text group-hover:text-white">{index + 1}. {category.name}</div>
+        <div className="inline-flex gap-1 items-center justify-end">
+          <ArrowRight className="relative w-0 -left-8 group-hover:text-white group-hover:w-fit group-hover:left-0 transition-all duration-200 ease-out" />
+        </div>
       </div>
     </Card>
   );
--- src/features/Home/components/CategorySection/categorySection.tsx
+++ src/features/Home/components/CategorySection/categorySection.tsx
@@ -6,8 +6,8 @@ const CategorySection = () => {
     <section className="min-h-screen w-full py-16 px-2 md:px-8 lg:px-16 space-y-4">
       <h2>Trending Categories</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 justify-center">
-        {categories.map((category) => (
-          <CategoryCard key={category.id} category={category} />
+        {categories.map((category, index) => (
+          <CategoryCard index={index} key={category.id} category={category} />
         ))}
       </div>
     </section>
