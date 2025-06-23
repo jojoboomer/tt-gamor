39207bed7e074427c7ef7586cf129e045b5f0c2c
jjlabrada <jjlabranate@gmail.com>
2025-06-23 16:22:34 -0400

    Fix AvatarStack image class for improved styling and consistency

--- src/features/Home/components/avatarStack.tsx
+++ src/features/Home/components/avatarStack.tsx
@@ -14,7 +14,7 @@ const AvatarStack = ({ list, className, size = "size-6" }: Props) => {
           key={index }
           src={item.avatar || createInitialsAvatar(item.username)}
           alt={item.username}
-          className={cn("rounded-full border border-border", size)}
+          className={cn("rounded-full bg-gray-600/60 shadow-md", size)}
         ></img>
       ))}
     </div>
