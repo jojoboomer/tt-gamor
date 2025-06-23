c74d7e664767a84469706d3041ff581fb7fa8013
jjlabrada <jjlabranate@gmail.com>
2025-06-23 16:23:20 -0400

    Add padding to EventList component for improved layout consistency

--- src/features/Home/components/HeroSection/FilterPanel/eventList.tsx
+++ src/features/Home/components/HeroSection/FilterPanel/eventList.tsx
@@ -3,7 +3,7 @@ import EventItem from "./eventItem";
 const EventList = ({ results }) => {
 
   return (
-    <ul className=" w-full h-full py-3 space-y-2 overflow-y-auto no-scrollbar flex-1 pb-20">
+    <ul className=" w-full h-full py-3 px-1 space-y-2 overflow-y-auto no-scrollbar flex-1 pb-20">
       {results.map((event, index) => (
         <EventItem
           key={event.id}
