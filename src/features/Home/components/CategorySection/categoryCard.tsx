43af2d1975e31c63230844aad8b14e493804c885
jjlabrada <jjlabranate@gmail.com>
2025-06-23 22:34:24 -0400

    Refactor CategoryCard component for improved styling consistency and hover effects

--- src/features/Home/components/CategorySection/categoryCard.tsx
+++ src/features/Home/components/CategorySection/categoryCard.tsx
@@ -16,12 +16,12 @@ const CategoryCard = ({ className, category, index }: Props) => {
       )}
       key={category.id}
     >
-      <div style={{ backgroundImage: `url(${category.box_art_url})` }} className={`absolute inset-0 opacity-0 group-hover:opacity-100 bg-center bg-cover transition-opacity duration-200`}></div>
-      <div className="absolute z-10 inset-0 bg-panel group-hover:bg-primary/40 transition-colors duration-200 ease-in-out"></div>
+      <div style={{ backgroundImage: `url(${category.box_art_url})` }} className={`absolute inset-0 md:opacity-0 md:group-hover:opacity-100 bg-center bg-cover transition-opacity duration-200`}></div>
+      <div className="absolute z-10 inset-0 md:bg-panel bg-primary/30 md:group-hover:bg-primary/40 transition-colors duration-200 ease-in-out"></div>
       <div className="w-full h-full z-10 flex flex-col gap-2 text-xl justify-between ">
-        <div className="text-text group-hover:text-white">{index + 1}. {category.name}</div>
+        <div className="font-semibold text-white md:text-text md:group-hover:text-white">{index + 1}. {category.name}</div>
         <div className="inline-flex gap-1 items-center justify-end">
-          <ArrowRight className="relative w-0 -left-8 group-hover:text-white group-hover:w-fit group-hover:left-0 transition-all duration-200 ease-out" />
+          <ArrowRight className="stroke-3 relative text-white md:w-0 left-0 w-fit md:-left-8 md:group-hover:text-white md:group-hover:w-fit md:group-hover:left-0 transition-all duration-200 ease-out" />
         </div>
       </div>
     </Card>
