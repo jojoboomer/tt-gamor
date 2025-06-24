f8e1f99767020719bc93988362680c4fb1cfbbed
jjlabrada <jjlabranate@gmail.com>
2025-06-23 22:24:52 -0400

    Fixed: Error rendering background image with rounded border

--- src/features/Home/components/CategorySection/categoryCard.tsx
+++ src/features/Home/components/CategorySection/categoryCard.tsx
@@ -11,12 +11,12 @@ const CategoryCard = ({ className, category, index }: Props) => {
   return (
     <Card
       className={cn(
-        "group relative rounded-lg overflow-hidden aspect-video w-full lg:w-auto h-40 flex-col-reverse items-end bg-center bg-cover hover:cursor-pointer  ",
+        "group relative rounded-lg overflow-hidden aspect-video w-full lg:w-auto h-40 flex-col-reverse items-end hover:cursor-pointer  ",
         className
       )}
-      style={{ backgroundImage: `url(${category.box_art_url})` }}
       key={category.id}
     >
+      <div style={{ backgroundImage: `url(${category.box_art_url})` }} className={`absolute inset-0 opacity-0 group-hover:opacity-100 bg-center bg-cover transition-opacity duration-200`}></div>
       <div className="absolute z-10 inset-0 bg-panel group-hover:bg-primary/40 transition-colors duration-200 ease-in-out"></div>
       <div className="w-full h-full z-10 flex flex-col gap-2 text-xl justify-between ">
         <div className="text-text group-hover:text-white">{index + 1}. {category.name}</div>
