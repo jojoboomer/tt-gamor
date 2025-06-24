348c74bbce3d6ae407f789e73e09f36bdedc2770
jjlabrada <jjlabranate@gmail.com>
2025-06-23 22:34:20 -0400

    Enhance h2 styling in CategorySection for improved typography

--- src/features/Home/components/CategorySection/categorySection.tsx
+++ src/features/Home/components/CategorySection/categorySection.tsx
@@ -4,7 +4,7 @@ import CategoryCard from "./categoryCard";
 const CategorySection = () => {
   return (
     <section className="min-h-screen w-full py-16 px-2 md:px-8 lg:px-16 space-y-4">
-      <h2>Trending Categories</h2>
+      <h2 className="font-semibold">Trending Categories</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 justify-center">
         {categories.map((category, index) => (
           <CategoryCard index={index} key={category.id} category={category} />
